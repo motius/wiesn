9bcf90d4311e60b6249c1dc7e845bbd7fc3c0426
Stuart Heap <sheap13@gmail.com>
2020-07-19 15:30:22 +0200

    OG calculator

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,57 @@
 <template>
   <div id="app">
-    <h1>{{greeting}}</h1>
+    <input v-model="volume" type="number" />
+    <h1>{{originalGravity}}</h1>
+    <h2>Malts</h2>
+    <div v-for="(maltInRecipe, index) in maltsInRecipe">
+      <h3>{{maltInRecipe.malt.name}}</h3>
+      <input @change="updateMaltMass(index, $event)" :value="maltInRecipe.mass" type="number" />
+    </div>
+    <select v-model="selectedMalt">
+      <option value=''>Select Malt</option>
+      <option v-for="availableMalt in availableMalts" :value="availableMalt.id">
+        {{availableMalt.name}}
+      </option>
+      <button @click="addSelectedMalt" :disabled="selectedMalt === ''">Add Malt</button>
+    </select>
   </div>
 </template>
 
 <script>
-import { greet } from './lib.rs'
-
-const greeting = greet()
+import { Recipe } from './lib.rs'
 
 export default {
   data: () => ({
-    message: 'Hello, world'
+    recipe: Recipe.new(),
+    selectedMalt: '',
   }),
+  methods: {
+    updateMaltMass: function(index, event) {
+      this.recipe = this.recipe.update_malt_mass(index, event.currentTarget.value)
+    },
+    addSelectedMalt: function() {
+      this.recipe = this.recipe.add_malt(this.selectedMalt)
+      this.selectedMalt = ''
+    }
+  },
   computed: {
-    greeting: () => greet()
-  }
+    volume: {
+      get: function() {
+        return this.recipe.get_volume()
+      },
+      set: function(newValue) {
+        this.recipe = this.recipe.set_volume(newValue)
+      },
+    },
+    originalGravity: function() {
+      return this.recipe.get_original_gravity()
+    },
+    maltsInRecipe: function() {
+      return JSON.parse(this.recipe.get_malts_in_recipe())
+    },
+    availableMalts: function() {
+      return JSON.parse(this.recipe.get_available_malts())
+    }
+  },
 }
 </script>
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,113 @@
 use wasm_bindgen::prelude::*;
+use serde_derive::Serialize;
+use serde_json::json;
 
-#[wasm_bindgen]
-pub fn greet() -> String {
-    "Hello wasm live".into()
+#[derive(Copy, Clone, Serialize)]
+struct Malt {
+    id: &'static str,
+    name: &'static str,
+    max_yield: f32, // pt / kg / L
+    color: f32, // Lovibond
 }
 
+#[derive(Copy, Clone, Serialize)]
+struct MaltInRecipe {
+    malt: Malt,
+    mass: f32,
+}
 
-#[no_mangle]
-pub fn add(a: i32, b: i32) -> i32 {
-    return a + b
+#[wasm_bindgen]
+struct Recipe {
+    malts_in_recipe: Vec<MaltInRecipe>,
+    volume: f32,
+}
+
+static malts: [Malt; 2] = [
+    Malt {
+        id: "2-row-barley",
+        name: "2 Row Barley",
+        max_yield: 370.0,
+        color: 2.0,
+    },
+    Malt {
+        id: "munich",
+        name: "Munich Malt",
+        max_yield: 350.0,
+        color: 10.0,
+    }
+];
+
+#[wasm_bindgen]
+impl Recipe {
+    pub fn new() -> Recipe {
+        let volume = 23.0;
+        let malts_in_recipe = vec![];
+
+        Recipe {
+            malts_in_recipe,
+            volume,
+        }
+    }
+
+    pub fn get_original_gravity(&self) -> f32 {
+        let total_yield = self.malts_in_recipe.iter().fold(0.0, |sum, malt| sum + malt.malt.max_yield * malt.mass);
+        1.0 + (total_yield / self.volume) / 1000.0
+    }
+
+    pub fn get_volume(&self) -> f32 {
+        self.volume
+    }
+
+    pub fn set_volume(&self, value: f32) -> Recipe {
+        let malts_in_recipe = self.malts_in_recipe.clone();
+        let volume = value;
+
+        Recipe {
+            malts_in_recipe,
+            volume,
+        }
+    }
+
+    pub fn get_malts_in_recipe(&self) -> String {
+        json!(self.malts_in_recipe).to_string()
+    }
+
+    pub fn update_malt_mass(&self, index: usize, new_mass: f32) -> Recipe {
+        let mut malts_in_recipe = self.malts_in_recipe.clone();
+        let volume = self.volume;
+
+        malts_in_recipe[index].mass = new_mass;
+
+        Recipe {
+            malts_in_recipe,
+            volume,
+        }
+    }
+
+    pub fn add_malt(&self, maltId: String) -> Recipe {
+        let mut malts_in_recipe = self.malts_in_recipe.clone();
+        let volume = self.volume;
+
+        return match malts.iter().find(|malt| malt.id == maltId) {
+            None => return Recipe {
+                malts_in_recipe,
+                volume,
+            },
+            Some(found_malt) => {
+                malts_in_recipe.push(MaltInRecipe {
+                    malt: found_malt.clone(),
+                    mass: 0.0,
+                });
+
+                return Recipe {
+                    malts_in_recipe,
+                    volume,
+                };
+            }
+        }
+    }
+
+    pub fn get_available_malts(&self) -> String {
+        json!(malts).to_string()
+    }
 }
